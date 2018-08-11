35427a6147a3e6764ff9188774553daf618273a1
unknown <abderrazzaqkhastaf@gmail.com>
2018-08-11 01:35:05 +0100

    Fixing bugs of stripe

--- database/migrations/2018_07_07_070218_create_orders_table.php
+++ database/migrations/2018_07_07_070218_create_orders_table.php
@@ -27,7 +27,7 @@ class CreateOrdersTable extends Migration
             $table->string('billing_phone');
             $table->string('billing_name_on_card');
             $table->integer('billing_discount')->default(0);
-            $table->string('billing_discount_code');
+            $table->string('billing_discount_code')->nullable();
             $table->integer('billing_subtotal');
             $table->integer('billing_tax');
             $table->integer('billing_total');
